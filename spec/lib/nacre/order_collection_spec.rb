550cc4fa39bb1461f2b0ca1f8e008900d6c91705
Damon Davison <damon@allolex.net>
2014-04-07 11:01:07 -0400

    Make OrderCollection spec test the right things
    OrderCollection was testing things in Order, so now it just makes sure it has an Order, which seems like a reasonable thing to depend on until we add more resources.

--- spec/lib/nacre/order_collection_spec.rb
+++ spec/lib/nacre/order_collection_spec.rb
@@ -2,40 +2,33 @@ require 'spec_helper'
 
 describe Nacre::OrderCollection do
 
-  describe '#orders' do
-
-    let(:parametrized_order_list) do
-      [
-        {
-          order_id: '123444',
-          parent_order_id: '555555'
-        }
-      ]
-    end
-
-    let(:subject) { Nacre::OrderCollection.new(parametrized_order_list) }
-
-    let(:order) { subject.members.first }
-
-    it 'should have members' do
-      expect(order.id).to eql('123444')
-      expect(order.parent_order_id).to eql('555555')
-    end
-
-    it_should_behave_like 'Enumerable'
+  let(:parametrized_order_list) do
+    [
+      {
+        order_id: '123444',
+        parent_order_id: '555555'
+      }
+    ]
   end
 
-  describe '.from_json' do
+  let(:subject) { Nacre::OrderCollection.new(parametrized_order_list) }
 
-    let(:orders_json) { fixture_file_content('order.json') }
-    let(:orders) { Nacre::OrderCollection.from_json(orders_json) }
+  it_should_behave_like 'Enumerable'
 
-    describe '.members' do
-      let(:order) { orders.members.first }
+  context 'initialization' do
 
-      it 'should be a list of order-like objects' do
-        expect(order.id).to eql('123456')
-        expect(order.parent_order_id).to eql('123455')
+    describe '.new' do
+      it 'should create a list of Orders' do
+        expect(subject.first).to be_a(Nacre::Order)
+      end
+    end
+
+    describe '.from_json' do
+      let(:orders_json) { fixture_file_content('order.json') }
+      let(:subject) { Nacre::OrderCollection.from_json(orders_json) }
+
+      it 'should be a list of Orders' do
+        expect(subject.first).to be_a(Nacre::Order)
       end
     end
   end
