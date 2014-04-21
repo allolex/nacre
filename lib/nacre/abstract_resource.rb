f8f49fd53fd1cc9ab44a99bb229e34f89b6e8e0e
Damon Davison <damon@allolex.net>
2014-04-21 15:04:13 -0400

    Extract search methods from AbstractResource into module

--- lib/nacre/abstract_resource.rb
+++ lib/nacre/abstract_resource.rb
@@ -43,16 +43,6 @@ module Nacre
       from_json(response.body)
     end
 
-    def self.find(id_list = [])
-      id_list = [id_list] unless id_list.kind_of?(Array)
-      request_url = build_search_url(
-        search_url,
-        "#{service_name}Id=#{id_list.join(',')}"
-      )
-      response = link.get(request_url)
-      SearchResults.from_json(response.body)
-    end
-
     private
 
     def self.service_name
@@ -109,10 +99,6 @@ module Nacre
 
     def self.url
       service_url + '/' + service_name
-    end
-
-    def self.search_url
-      service_url + '/' + service_name + '-search'
     end
 
     def self.link
---
+++ lib/nacre/concerns/searchable.rb
@@ -0,0 +1,20 @@
+module Nacre::Searchable
+
+  def find(id_list = [])
+    id_list = [id_list] unless id_list.kind_of?(Array)
+    request_url = build_search_url(
+      search_url,
+      "#{service_name}Id=#{id_list.join(',')}"
+    )
+    response = link.get(request_url)
+    Nacre::SearchResults.from_json(response.body)
+  end
+
+  private
+
+  def search_url
+    service_url + '/' + service_name + '-search'
+  end
+
+
+end
