bff8017d0633cf747ff828c93608191fb8ff5d74
Damon Davison <damon@allolex.net>
2014-04-24 11:55:10 -0400

    Add ColumnProperty class for search results

---
+++ lib/nacre/column_property.rb
@@ -0,0 +1,17 @@
+require 'nacre/abstract_resource'
+
+module Nacre
+  class ColumnProperty < AbstractResource
+    include Inflectible
+
+    attribute :name
+    attribute :sortable
+    attribute :filterable
+    attribute :report_data_type
+    attribute :required
+
+    def formatted_name
+      snake_case(name).to_sym
+    end
+  end
+end
