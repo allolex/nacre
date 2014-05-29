f3dcdc174eabc03ac23e0c9e1ff730f1026954db
Damon Davison <damon@allolex.net>
2014-05-29 16:14:19 -0400

    Abstract behavior into AbstractResourceCollection

--- lib/nacre/order_collection.rb
+++ lib/nacre/order_collection.rb
@@ -1,60 +1,13 @@
-require 'nacre/concerns/inflectible'
+require 'nacre/abstract_resource_collection'
 
 module Nacre
 
-  class OrderCollection < AbstractCollection
-
-    include Enumerable
-    extend Inflectible
-    extend Getable
-
-    attr_accessor :members
-
-    def initialize(parametrized_resources = [])
-      self.members = []
-      parametrized_resources.each do |r|
-        resource_params = normalize_params(r)
-        self.members << self.class.resource_class.new(resource_params)
-      end
-    end
-
-    def each(&block)
-      members.each do |member|
-        block.call(member)
-      end
-    end
-
-    def self.from_json(json)
-      resources = extract_resources(json)
-      collection = new
-      collection.members = resources.map { |r| resource_class.new( json_to_params(r) ) }
-      collection
-    end
+  class OrderCollection < AbstractResourceCollection
 
     private
 
-    def normalize_params(params)
-      resource_params = params
-      id_key = "#{self.class.service_name}_id".to_sym
-      resource_params[:id] = params[id_key]
-      resource_params.delete(id_key)
-      resource_params
-    end
-
     def self.resource_class
       Order
-    end
-
-    def self.extract_resources(json)
-      JSON.parse(json)['response']
-    end
-
-    def self.json_to_params(json_order)
-      params = {}
-      %w/id parentOrderId/.each do |key|
-        params[snake_case(key).to_sym] = json_order[key]
-      end
-      params
     end
   end
 end
