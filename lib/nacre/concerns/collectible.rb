8e7af127cba7bb28035d2d92486f2b3a7b4600fc
Damon Davison <damon@curatur.com>
2017-01-31 11:34:19 -0500

    Use nested module block

--- lib/nacre/concerns/collectible.rb
+++ lib/nacre/concerns/collectible.rb
@@ -1,31 +1,33 @@
-module Nacre::Collectible
-  def self.included(base)
-    base.class_exec do
-      def initialize(resource_list = [])
-        @members = []
+module Nacre
+  module Collectible
+    def self.included(base)
+      base.class_exec do
+        def initialize(resource_list = [])
+          @members = []
 
-        resource_list.each do |resource_params|
-          members << self.class.resource_class.new(resource_params)
+          resource_list.each do |resource_params|
+            members << self.class.resource_class.new(resource_params)
+          end
         end
       end
     end
-  end
 
-  def members
-    @members
-  end
-
-  def members=(list)
-    @members = list
-  end
-
-  def each(&block)
-    members.each do |member|
-      block.call(member)
+    def members
+      @members
     end
-  end
 
-  def params
-    members.map(&:params)
+    def members=(list)
+      @members = list
+    end
+
+    def each(&block)
+      members.each do |member|
+        block.call(member)
+      end
+    end
+
+    def params
+      members.map(&:params)
+    end
   end
 end
