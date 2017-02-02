19eb2e901528ae661d04bf39b7eebd3a858a4d3d
Damon Davison <damon@curatur.com>
2017-02-02 15:23:10 -0500

    Pull Enumerable inclusion up into Collectible (#35)

--- lib/nacre/concerns/collectible.rb
+++ lib/nacre/concerns/collectible.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 module Nacre
   module Collectible
+    include Enumerable
+
     def self.included(base)
       base.class_exec do
         def initialize(resource_list = []) # rubocop:disable NestedMethodDefinition
--- lib/nacre/order/invoice_collection.rb
+++ lib/nacre/order/invoice_collection.rb
@@ -3,7 +3,6 @@ require 'nacre/concerns/collectible'
 module Nacre
   class Order::InvoiceCollection
     include Collectible
-    include Enumerable
 
     def self.resource_class
       Nacre::Order::Invoice
